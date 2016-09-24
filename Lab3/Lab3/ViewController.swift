6f43f13efacbaff30e9fcd45b7535f8adba1158b
Rabin Ranabhat <rabinranabhat@Rabins-Mac-mini.local>
2016-09-24 11:07:12 -0600

    Completed the form. Added the function to the clear button. TODO: cancel and okay buttons.

--- Lab3/Lab3/ViewController.swift
+++ Lab3/Lab3/ViewController.swift
@@ -16,9 +16,30 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     @IBOutlet weak var studentPageLabel: UILabel!
     @IBOutlet weak var studentDOB: UITextField!
     @IBOutlet weak var studentEthnicity: UITextField!
+    @IBOutlet weak var studentConsentDate: UITextField!
+    @IBOutlet weak var studentIEP: UITextField!
+    @IBOutlet weak var studentIEPSwitch: UISwitch!
+    @IBOutlet weak var student401Report: UITextField!
+    @IBOutlet weak var student401Switch: UISwitch!
     
     //loading the ethnicity values
     var ethnicities = ["None","Caucasian", "African American"]
+
+    @IBAction func clearFields(sender: UIButton) {
+    
+        studentID.text = ""
+        studentFirstname.text = ""
+        studentLastname.text = ""
+        studentDOB.text = ""
+        studentEthnicity.text = ""
+        studentConsentDate.text = ""
+        studentIEP.text = ""
+        student401Report.text = ""
+        
+        //resetting the buttons as well
+        studentIEPSwitch.setOn(false, animated: true)
+        student401Switch.setOn(false, animated: true)
+    }
     
     func datePickerValueChanged(sender: UIDatePicker)
     {
@@ -36,7 +57,47 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         datePickerView.addTarget(self, action: #selector(ViewController.datePickerValueChanged), forControlEvents: UIControlEvents.ValueChanged)
         
     }
+    
+    func consentDateValueChanged(sender: UIDatePicker)
+    {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
+        dateFormatter.timeStyle = NSDateFormatterStyle.NoStyle
+        studentConsentDate.text = dateFormatter.stringFromDate(sender.date)
+    }
+    
+    @IBAction func enterConsent(sender: UITextField) {
+        let datePickerView:UIDatePicker = UIDatePicker()
+        
+        datePickerView.datePickerMode = UIDatePickerMode.Date
+        sender.inputView = datePickerView
+        datePickerView.addTarget(self, action: #selector(ViewController.consentDateValueChanged), forControlEvents: UIControlEvents.ValueChanged)
+        
+    }
 
+    @IBAction func IEPSwitchClicked(sender: UISwitch) {
+    
+        if studentIEPSwitch.on
+        {
+            studentIEP.enabled = true
+        }
+        else
+        {
+            studentIEP.enabled = false
+        }
+    }
+
+    @IBAction func SwitchClicked(sender: UISwitch) {
+        
+        if student401Switch.on
+        {
+            student401Report.enabled = true
+        }
+        else
+        {
+            student401Report.enabled = false
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,6 +105,25 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         
         //setting the title of the page to bold
         studentPageLabel.font = UIFont.boldSystemFontOfSize(17)
+        
+        //reading the switches to determine the enabled states of the text fields
+        if studentIEPSwitch.on
+        {
+            studentIEP.enabled = true
+        }
+        else
+        {
+            studentIEP.enabled = false
+        }
+        
+        if student401Switch.on
+        {
+            student401Report.enabled = true
+        }
+        else
+        {
+            student401Report.enabled = false
+        }
         
         let ethnicityPicker = UIPickerView()
         ethnicityPicker.delegate = self
@@ -83,20 +163,24 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
             studentID.resignFirstResponder()
         }
         
-        if(studentFirstname != nil)
+        if studentFirstname != nil
         {
             studentFirstname.resignFirstResponder()
         }
         
-        if(studentLastname != nil)
+        if studentLastname != nil
         {
             studentLastname.resignFirstResponder()
         }
         
         studentDOB.resignFirstResponder()
         studentEthnicity.resignFirstResponder()
+        studentConsentDate.resignFirstResponder()
+        student401Report.resignFirstResponder()
+        studentIEP.resignFirstResponder()
     }
     
+    //this will listen to all the text fields and then resign the control to keyboard when 'DONE' is tapped
     @IBAction func textFieldDoneEditing(sender: UITextField)
     {
         sender.resignFirstResponder()
